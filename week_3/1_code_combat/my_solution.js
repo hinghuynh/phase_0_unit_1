8fd38a1c2cfc34443ca19bf453e90e9d7709f9f5
hinghuynh <hinghuynh@gmail.com>
2014-04-14 17:05:03 -0700

    Update my_solution.js

--- week_3/1_code_combat/my_solution.js
+++ week_3/1_code_combat/my_solution.js
@@ -5,7 +5,99 @@
 // command + / This will comment the line. 
  
  
- 
+ //RESCUE MISSION
+//move down x2
+this.moveDown();
+this.moveDown();
+//move right x1
+this.moveRight();
+// move up x2
+this.moveUp();
+this.moveUp();
+// move right x2
+this.moveRight();
+this.moveRight();
+//move down x1
+this.moveDown();
+//Attack!
+this.attackNearbyEnemy();
+
+
+//Grab the mushroom
+//move up x1
+this.moveUp();
+//move right x1
+this.moveRight();
+//move left x1
+this.moveLeft();
+//move up x1
+this.moveUp();
+//Attack!
+this.attackNearbyEnemy();
+
+//Drink move
+//move right x1
+this.moveRight();
+//Attack!
+this.attackNearbyEnemy();
+//move right x1
+this.moveRight();
+//move down x1
+this.moveDown();
+//move up x1
+this.moveUp();
+//move right x1
+this.moveRight();
+//Attack!
+this.attackNearbyEnemy();
+
+//Taunt the guards
+//move right x1
+this.moveRight();
+//bust down the door
+this.bustDownDoor();
+//move right x1
+this.moveRight();
+//say "hey there"
+this.say("Hey there!");
+//move left 2x
+this.moveLeft();
+this.moveLeft();
+//say Attack!
+this.say("Attack!");
+//move right
+this.moveRight();
+//say "Follow me"
+this.say("Follow me.");
+//move right x2
+this.moveRight();
+this.moveRight();
+//move  up x1
+this.moveUp();
+//move right x1
+this.moveUp();
+//move down x1
+this.movedDown();
+//move right
+this.moveRight();
+//say "hey there"
+this.say("Hey there!");
+//move left x1
+this.moveLeft();
+//say "Attack!" x2
+this.say("Attack!");
+this.say("Attack!");
+//say "Follow me"
+this.say("Follow me.");
+//move right x1
+this.moveRight();
+//move up x1
+this.moveUp();
+//move right x1
+this.moveRight();
+
+// its-a-trap
+
  
  
  
